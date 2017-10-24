cde911028dd822d5b940162cfc5505913209486d
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2017-10-24 15:59:09 +0900

    went back and redid the 1-4hw assignment to get more practice with GET and POST requests

--- Week 1/hw1-4/app.js
+++ Week 1/hw1-4/app.js
@@ -8,13 +8,13 @@ var express = require('express'),
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Handler for internal server errors
 function errorHandler(err, req, res, next) {
-    console.error(err.message);
-    console.error(err.stack);
-    res.status(500).render('error_template', { error: err });
+  console.error(err.message)
+  console.error(err.stack)
+  res.status(500)
+  res.render('error_template', { error: err })
 }
 
 MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
@@ -22,33 +22,33 @@ MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
     assert.equal(null, err);
     console.log("Successfully connected to MongoDB.");
 
-    app.get('/', function(req, res, next) {
-        res.render('add_movie', {});
+    app.get('/', function(req, res, next){
+        res.render('add_movie', {})
     });
-    
+
     app.post('/add_movie', function(req, res, next) {
-        var title = req.body.title;
-        var year = req.body.year;
-        var imdb = req.body.imdb;
-        
-        if ((title == '') || (year == '') || (imdb == '')) {
-            next('Please provide an entry for all fields.');
-        } else {
-            db.collection('movies').insertOne(
-                { 'title': title, 'year': year, 'imdb': imdb },
-                function (err, r) {
-                    assert.equal(null, err);
-                    res.send("Document inserted with _id: " + r.insertedId);
-                }
-            );
-        }
-    });
-    
-    app.use(errorHandler);
-    
+      title = req.body.title
+      year = req.body.year
+      imdb = req.body.imdb
+
+      if ((title === '') || (year === '') || (imdb === '')) {
+        next('Please provide an entry for all fields.')
+      } else {
+        db.collection('movies').insertOne(
+          { 'title': title, 'year': year, 'imdb': imdb },
+          function(err, r) {
+            assert.equal(null, err)
+            res.send("Document inserted with _id: " + r.insertedId)
+          }
+        )
+      }
+    })
+
+    app.use(errorHandler)
+
     var server = app.listen(3000, function() {
         var port = server.address().port;
         console.log('Express server listening on port %s.', port);
     });
-    
+
 });
