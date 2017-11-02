511fa5deffd3da167f8b8e393825701cc96e9984
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2017-11-02 22:35:38 +0900

    completed regex handout

--- week-3/regexOperatorInNodeJSDriver/app.js
+++ week-3/regexOperatorInNodeJSDriver/app.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient,
-    commandLineArgs = require('command-line-args'), 
+    commandLineArgs = require('command-line-args'),
     assert = require('assert');
 
 
@@ -10,13 +10,13 @@ MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
 
     assert.equal(err, null);
     console.log("Successfully connected to MongoDB.");
-    
+
     var query = queryDocument(options);
     var projection = projectionDocument(options);
 
     var cursor = db.collection('companies').find(query);
     cursor.project(projection);
-    
+
     var numMatches = 0;
 
     cursor.forEach(
@@ -38,15 +38,20 @@ MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
 function queryDocument(options) {
 
     console.log(options);
-    
+
     var query = {};
 
     if ("overview" in options) {
-        query.overview = {"$regex": options.overview, "$options": "i"};
+        query.overview = {"$regex": options.overview, "$options": "i"}; // i for case insensitive
     }
-    
+
+    if ("milestones" in options) {
+        query["milestones.source_description"] =
+        {"$regex": options.milestones, "$options": "i"}
+    }
+
     return query;
-    
+
 }
 
 
@@ -55,9 +60,16 @@ function projectionDocument(options) {
     var projection = {
         "_id": 0,
         "name": 1,
-        "founded_year": 1,
-        "overview": 1
+        "founded_year": 1
     };
+
+    if ("overview" in options) {
+      projection.overview = 1
+    }
+
+    if ("milestones" in options) {
+      projection["milestones.source_description"] = 1
+    }
 
     return projection;
 }
@@ -66,9 +78,10 @@ function projectionDocument(options) {
 function commandLineOptions() {
 
     var cli = commandLineArgs([
-        { name: "overview", alias: "o", type: String }
+        { name: "overview", alias: "o", type: String },
+        { name: "milestones", alias: "m", type: String }
     ]);
-    
+
     var options = cli.parse()
     if (Object.keys(options).length < 1) {
         console.log(cli.getUsage({
@@ -79,7 +92,5 @@ function commandLineOptions() {
     }
 
     return options;
-    
+
 }
-
-
